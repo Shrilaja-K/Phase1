4c7f480369b77a795fe2cdf008fd492e980b4aa0
Shrilaja-K <shrilaja.k@rently.com>
2025-10-31 08:55:03 +0530

    Deleted

--- Meal-app/src/Components/Footer.tsx
+++ Meal-app/src/Components/Footer.tsx
@@ -20,17 +20,6 @@ class Footer extends Component {
           <Typography variant="body2" sx={{ mb: 1 }}>
             &copy; {new Date().getFullYear()} Meal App. All rights reserved.
           </Typography>
-          <Box>
-            <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
-              Home
-            </Link>
-            <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
-              Trending Meals
-            </Link>
-            <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
-              Sweets
-            </Link>
-          </Box>
         </Container>
       </Box>
     );
